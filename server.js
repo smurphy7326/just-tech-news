ccd527445c24fc0fb2c4e39cbe614f7917be5503
smurphy7326 <smurphy7326@gmail.com>
2021-10-20 17:27:37 -0400

    14.2.6

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,12 +11,23 @@ const sequelize = require('./config/connection');
 
 const hbs = exphbs.create({});
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 app.use(require('./controllers/'));
 
